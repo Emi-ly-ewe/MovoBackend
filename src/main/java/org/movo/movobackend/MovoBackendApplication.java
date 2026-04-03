2b3dffa847838510f1305bc2c806053cfbce45ae
Emily <tdg2021emiliaargibay@gmail.com>
2026-04-03 02:58:23 +0100

    Commit Inicial. Creación del proyecto

---
+++ src/main/java/org/movo/movobackend/MovoBackendApplication.java
@@ -0,0 +1,13 @@
+package org.movo.movobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovoBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovoBackendApplication.class, args);
+    }
+
+}
